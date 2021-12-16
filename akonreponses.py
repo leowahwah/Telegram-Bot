ad0f183c9d33e4491c5dcc0286d813967b1709b3
leowahwah <96245797+leowahwah@users.noreply.github.com>
2021-12-16 22:49:07 +0800

    Update akonreponses.py

--- akonreponses.py
+++ akonreponses.py
@@ -1,9 +1,9 @@
 def hello(input):
     user_message=str(input).lower()
 
-    if ("keyword" in user_message):
+    if ("keyword" in user_message): # Once the keyword is detected in a message 
 
-       return 'Any response you like'
+       return 'Any response you like' # Your bot will reply to the message with this response
 
 
     
