d92f1a641a01988010301ee97ff1fea65ceb455b
leowahwah <96245797+leowahwah@users.noreply.github.com>
2021-12-17 02:39:06 +0800

    Update akonapi.main.py

--- akonapi.main.py
+++ akonapi.main.py
@@ -11,7 +11,7 @@ def error(update,context):
     print(f"Update{update}caused error{context.error}")
 
 def main():
-    updater = Updater(mykey.API_KEY, use_context=True)
+    updater = Updater(API_KEY, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(MessageHandler(Filters.text, handle_message))
     dp.add_error_handler(error)
