2aff673c80c1f304fd3aa96eb68e6f20c55cec1f
leowahwah <96245797+leowahwah@users.noreply.github.com>
2021-12-17 13:54:21 +0800

    Update sample_api.main.py

--- sample_api.main.py
+++ sample_api.main.py
@@ -1,13 +1,27 @@
 from telegram.ext import*  # You need to install python-telegram-bot in your IDE before you can import from telegram.ext
-import responses as R
+
 
 API_KEY="xxxxxxxxxxxxxxxx" # The API key should be a string given to you by Bot Father
 
 
 def handle_message(update, context):
     text= str(update.message.text).lower() 
-    reponse=R.respond(text) # The respond function is defined in the responses pyfile
+    reponse=respond(text) # The respond function is defined in the responses pyfile
     update.message.reply_text(reponse)
+    
+    
+def respond(input):
+    user_message = str(input).lower() # Make sure the bot won't miss the message due to capitalisation
+
+    if "keyword" in user_message : # Once the keyword is detected in a message 
+
+       return 'Any response you like' # Your bot will reply to the message with this response
+
+
+    if "Other keyword" in user_message : # Once the keyword is detected in a message 
+
+       return 'Another response you like' # Your bot will reply to the message with this response
+
 
 def error(update,context): # Just in case any error occurs
     print(f"Update{update}caused error{context.error}")
